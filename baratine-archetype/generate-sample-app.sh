4d2723e5a110d79b242f466f28d58bf31d5b121a
alex <rojkov@gmail.com>
2015-03-31 15:55:18 -0700

    fix formatting

--- baratine-archetype/generate-sample-app.sh
+++ baratine-archetype/generate-sample-app.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-mvn archetype:generate -o -DarchetypeGroupId=io.baratine \
+mvn archetype:generate -DarchetypeGroupId=io.baratine \
 -DarchetypeArtifactId=maven-archetype-baratine \
--DgroupId=my.group -DartifactId=MyArtifact \
+-DgroupId=org.acme -DartifactId=MyApp \
 -DarchetypeVersion=1.0-SNAPSHOT \
 -DinteractiveMode=false
